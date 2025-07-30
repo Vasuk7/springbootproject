ebc27f142d820d9ecf9a35dd52ecb656d9b46872
Vasuk7 <122456668+Vasuk7@users.noreply.github.com>
2025-07-30 13:46:25 +0530

    updated

---
+++ src/main/java/com/vkproject/tutorial/VksbprojectApplication.java
@@ -0,0 +1,13 @@
+package com.vkproject.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VksbprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VksbprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkproject/tutorial/VksbprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkproject.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VksbprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
